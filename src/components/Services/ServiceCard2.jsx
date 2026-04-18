4bb4808bf4d9ee2072f4d42aa22d33eaf1d9c4bf
Payal <payall01261@gmail.com>
2026-04-18 09:44:18 +0530

    Refactor ServiceCard2 styles and add text color
    Updated ServiceCard2 component styles and structure.

--- src/components/Services/ServiceCard2.jsx
+++ src/components/Services/ServiceCard2.jsx
@@ -2,11 +2,11 @@
 
 const ServiceCard2 = ({title, description,skills, index}) => {
    return (
-    <div className='text-black h-[120vw] w-full bg-black/10 rounded-2xl px-[8vw] p-[5vw]'>
-      <h4 className='text-[4vw]'>{index}</h4>
+    <div className='text-black h-[120vw] w-full bg-black/10 rounded-2xl px-[8vw] pt-[9vw] p-[5vw]'>
+      
       <h1 className='text-[12vw] font-medium leading-[10.5vw] ml-[1vw] mt-[6vw]'>{title}</h1>
       <h3 className='text-[4.3vw] mt-[2vw] ml-[1vw]'>{description}</h3>
-      <h5 className='text-[6vw] mt-[6vw] ml-[1vw]'>categories</h5>
+      <h5 className='text-[6vw] mt-[6vw] ml-[1vw] text-orange-800'>categories</h5>
       <div className='flex flex-col mt-[3vw] gap-[3vw] ml-[1vw]'>
         {skills.map((elem, idx) => (
 
