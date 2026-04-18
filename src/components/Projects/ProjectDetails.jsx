7ad7bc5863c8497348fcdbbb2d0b3166db783b13
Payal <payall01261@gmail.com>
2026-04-18 14:02:12 +0530

    Adjust project details layout and positioning

--- src/components/Projects/ProjectDetails.jsx
+++ src/components/Projects/ProjectDetails.jsx
@@ -65,12 +65,12 @@ const ProjectDetails = () => {
   return (
     <div className="min-h-screen w-screen text-white/70 relative p-[7vw]">
             <div className="absolute left-[4.5vw] top-[17.4vw] lg:top-[4.2vw] bg-red-800 h-[3vw] w-[0.6vw] lg:h-[0.8vw] lg:w-[0.16vw]"></div>
-            <h2 ref={numRef} className="absolute lg:text-[1vw] text-[3.2vw] left-[11.6] lg:left-[5vw] top-[17vw] lg:top-[4vw]">Project {project.id}</h2>
+            <h2 ref={numRef} className="absolute lg:text-[1vw] text-[3.2vw] left-[17.6] lg:left-[5vw] top-[17vw] lg:top-[4vw]">Project {project.id}</h2>
             <h4 ref={nameRef} className="text-[7vw] lg:text-[2.4vw] mt-[27vw] lg:mt-[4vw]">{project.projectName}</h4>
             <p ref={descriptionRef} className="text-[3.2vw] lg:text-[1vw] text-white/60 lg:text-white/70 font-light mt-[11vw] w-[73vw] lg:w-[53vw] leading-[3.2vw] lg:leading-[1.4vw] lg:tracking-wide">{project.projectDescription}</p>
             
             
-            <h4 className="lg:block hidden absolute top-[39%] lg:top-[27%] left-[50%] translate-x-[-50%] text-[8vw] lg:text-[7vw] tracking-widest opacity-50 lg:opacity-40">{project.bgname}</h4>
+            <h4 className="lg:block hidden absolute top-[39%] lg:top-[27%] left-[10%] lg:left-[50%] translate-x-[-50%] text-[8vw] lg:text-[7vw] tracking-widest opacity-50 lg:opacity-40">{project.bgname}</h4>
             <Link to={project.image}> 
             <img  className="lg:h-[42vw] lg:w-[72vw] h-[52vw] w-[80vw]translate-x-[-50%] object-cover object-center mt-[18vw] lg:mt-[12vw] opacity-80 relative left-[50%] rounded-xl" src={project.image} alt="" />
             </Link>
